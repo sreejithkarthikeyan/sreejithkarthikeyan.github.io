a1df76003937bbc8c3e9951c656953cb842a4261
SREEJITH KS <sreeksleo@gmail.com>
2024-10-25 13:52:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 
+// Wait for the page to load fully
+window.onload = function() {
+    gsap.from("header", { duration: 1, y: -100, opacity: 0, ease: "bounce" });
+
+    gsap.from("#about", { 
+        scrollTrigger: "#about", 
+        duration: 1, 
+        x: -100, 
+        opacity: 0, 
+        ease: "power2.inOut" 
+    });
+    
+    gsap.from("#projects", { 
+        scrollTrigger: "#projects", 
+        duration: 1, 
+        x: 100, 
+        opacity: 0, 
+        ease: "power2.inOut" 
+    });
+    
+    gsap.from("#contact", { 
+        scrollTrigger: "#contact", 
+        duration: 1, 
+        y: 100, 
+        opacity: 0, 
+        ease: "power2.inOut" 
+    });
+};
